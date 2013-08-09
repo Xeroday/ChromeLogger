de6985bfde1370081b00538f903aecbf55ce1f57
Xeroday <eric@xeroday.org>
2013-08-09 18:30:45 -0400

    Add options page & formatting

--- extension/viewer.js
+++ extension/viewer.js
@@ -3,15 +3,14 @@ var app = angular.module('app', ['truncate']);
 function MainCtrl($scope) {
   $scope.load = function() {
     $scope.logs = [];
-    var start = Date.parse($('#picker').val());
-    var end = new Date(Date.parse($('#picker').val()) + 86400000).getTime();
+    var start = Date.parse($('#picker').val()); // Convert to unix time
+    var end = new Date(Date.parse($('#picker').val()) + 86400000).getTime(); // Convert to unix time and add a day
     chrome.storage.local.get(function(x) {
       for (var key in x) {
         if (key > start && key < end) { 
           $scope.logs.push([key, x[key].split('^~^')]);
         }
       }
-      $('.tip').tooltip();
       $scope.$apply();
     });
   }
