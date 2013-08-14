4e773a38dd6d6b621cbd457049fa943aad2244ab
Xeroday <eric@xeroday.org>
2013-08-13 22:21:32 -0400

    Change to ui bootstrap datepicker

--- extension/viewer.js
+++ extension/viewer.js
@@ -1,19 +1,29 @@
 var app = angular.module('app', ['truncate', 'ui.bootstrap']);
 
 function MainCtrl($scope) {
+  
+  $scope.datepicker = new Date(new Date().setHours(0, 0, 0));
+  $scope.maxDate = new Date();
+
   $scope.load = function() {
     $scope.logs = [];
-    var start = Date.parse($('#picker').val()); // Convert to unix time
-    var end = new Date(Date.parse($('#picker').val()) + 86400000).getTime(); // Convert to unix time and add a day
+    var start = Date.parse($scope.datepicker); // Convert to unix time
+    var end = new Date(Date.parse($scope.datepicker) + 86400000).getTime(); // Convert to unix time and add a day
+    var min = new Date().getTime(); // Will be used to get min date
     chrome.storage.local.get(function(x) {
       for (var key in x) {
+        if (key < min) { // Get earliest log
+          min = key;
+        }
         if (key > start && key < end) { 
           $scope.logs.push([key, x[key].split('^~^')]);
         }
       }
+      $scope.minDate = new Date(parseInt(min)); // Convert min to Date object
       $scope.$apply();
     });
   }
+  $scope.load();
 
   chrome.storage.local.getBytesInUse(function(x) {
     $scope.usageMB = (x / 1048576 + '').substring(0, 5);
